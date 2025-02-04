9d877f8cff7dc4e67a2a5608b9c1c5da9bebab43
Jack Hodkinson <jackhodkinson93@gmail.com>
2025-02-04 17:46:16 +0000

    Parallelize eval and calculate pass@k

--- fim_eval/evaluate_fim_results.py
+++ fim_eval/evaluate_fim_results.py
@@ -1,13 +1,21 @@
 import gzip
+import itertools
 import json
 import os
+import tqdm
+from typing import Union, List
+from collections import defaultdict
+
+import requests
+from rich.console import Console
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import numpy as np
 
 from fim_eval.execution import check_correctness
 from fim_eval.load_problems import Problem
 from fim_eval.result import Result as Sample
 
-import requests
-from rich.console import Console
+MAX_WORKERS = 8
 
 console = Console()
 
@@ -58,25 +66,68 @@ def evaluate_results(samples: list[Sample], problems: list[Problem]):
     print(f"Evaluating {len(samples)} samples and {len(problems)} problems...")
 
 
+def estimate_pass_at_k(
+    num_samples: Union[int, List[int], np.ndarray],
+    num_correct: Union[List[int], np.ndarray],
+    k: int,
+) -> np.ndarray:
+    """
+    Estimates pass@k of each problem and returns them in an array.
+    """
+
+    def estimator(n: int, c: int, k: int) -> float:
+        """
+        Calculates 1 - comb(n - c, k) / comb(n, k).
+        """
+        if n - c < k:
+            return 1.0
+        return 1.0 - np.prod(1.0 - k / np.arange(n - c + 1, n + 1))
+
+    if isinstance(num_samples, int):
+        num_samples_it = itertools.repeat(num_samples, len(num_correct))
+    else:
+        assert len(num_samples) == len(num_correct)
+        num_samples_it = iter(num_samples)
+
+    return np.array(
+        [estimator(int(n), int(c), k) for n, c in zip(num_samples_it, num_correct)]
+    )
+
+
 if __name__ == "__main__":
     download_eval()
     problems = load_eval()
     samples = load_samples()
     problem_by_id = {problem.task_id: problem for problem in problems}
 
-    results = []
-    for sample in samples:
-        problem = problem_by_id[sample.task_id]
-        result = check_correctness(problem.model_dump(), sample.completion, 10)
-        results.append(result)
-
-        if not result["passed"]:
-            console.print(
-                "-" * 30,
-                "\n",
-                f"{problem.prompt}[yellow on grey23]{sample.completion}[/yellow on grey23]{problem.suffix}",
-                "\n",
-                "-" * 30,
+    results_by_id = defaultdict(list)
+    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+        futures = []
+        for sample in samples:
+            future = executor.submit(
+                check_correctness,
+                problem_by_id[sample.task_id].model_dump(),
+                sample.completion,
+                10,
             )
+            futures.append(future)
 
+        for future in tqdm.tqdm(as_completed(futures), total=len(futures)):
+            task_id = future.result()["task_id"]
+            results_by_id[task_id].append(future.result())
+
+    # flatten results
+    results = [result for results in results_by_id.values() for result in results]
     print(f"Accuracy: {sum(result['passed'] for result in results) / len(results)}")
+
+    num_attempts_per_problem = [len(results) for results in results_by_id.values()]
+    num_successes_per_problem = [
+        sum(result["passed"] for result in results)
+        for results in results_by_id.values()
+    ]
+    pass_at_k = estimate_pass_at_k(
+        num_attempts_per_problem,
+        num_successes_per_problem,
+        k=1,
+    ).mean()
+    print(f"Pass@1: {pass_at_k}")
